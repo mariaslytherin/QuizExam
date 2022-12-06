83c427544fb1e277b165bffe7c8c3a5bb80ac0e8
Maria Pirinova <maria.pirinova39@gmail.com>
2022-12-06 23:42:45 +0200

    Add string to guid extension method

---
+++ QuizExam.Core/Extensions/GuidExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace QuizExam.Core.Extensions
+{
+    public static class GuidExtension
+    {
+        public static Guid ToGuid(this string guid)
+        {
+            return Guid.Parse(guid.ToLower());
+        }
+    }
+}
