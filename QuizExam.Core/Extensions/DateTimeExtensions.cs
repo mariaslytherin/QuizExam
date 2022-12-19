3867ffe2f5036de7c7dab23d1ad5773b9abd6aac
Maria Pirinova <maria.pirinova39@gmail.com>
2022-12-19 15:45:22 +0200

    Add summary for datetime to string extension method

--- QuizExam.Core/Extensions/DateTimeExtensions.cs
+++ QuizExam.Core/Extensions/DateTimeExtensions.cs
@@ -2,6 +2,12 @@
 {
     public static class DateTimeExtensions
     {
+        /// <summary>
+        /// Converts the value of the current DateTime object to its equivalent string
+        //  representation using the current specified format (dd/MM/yyyy).
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns>A string representation of value of the current DateTime object.</returns>
         public static string ToDateOnlyString(this DateTime date)
         {
             return date.ToString("dd/MM/yyyy");
